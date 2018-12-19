f16cca874047bf2bc0c2efe8c103217d3dc08938
Lingwei Luo <564258080@qq.com>
2018-12-19 19:22:52 +0800

    HousePrice

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-dic={0:'A',1:'B',2:'C',3:'D'}
-frame=pd.DataFrame(np.random.randn(4,4),columns=['A','B','C','D'])
-series=pd.Series(np.arange(4))
-T=series.map(dic)
-print(T)
